98ce0547a8f1b9b749a3bf0c20880c1044870049
dafkwame <dafkwametay@yahoo.com>
2023-05-08 09:13:28 -0700

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it to STDOUT.
+ * @filename: The file name
+ * @letters: number of letters that can be printed
+ * Return: the actual number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *Ewura;
+	ssize_t Nana;
+	ssize_t Adwoa;
+	ssize_t Ama;
+
+	Nana = open(filename, O_RDWR);
+	if (filename == NULL)
+		return (0);
+	Ewura = malloc(sizeof(char) * letters);
+	Ama = read(Nana, Ewura, letters);
+	Adwoa = write(STDOUT_FILENO, Ewura, Ama);
+
+	free(Ewura);
+	close(Nana);
+	return (Adwoa);
+}
